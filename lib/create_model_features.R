bf5ae42044cb69b5cf047ef5a02631772ad9fb8e
henriquepgomide <henriquepgomide@gmail.com>
2017-07-02 18:28:38 -0300

    Fix player data from Cartola API under way.

--- lib/create_model_features.R
+++ lib/create_model_features.R
@@ -1,10 +1,45 @@
 # TODO
-# Create variables for each round:
-  # ((Number of goals scored) / (N of matches))  ## full schedule; last 3 rounds;
-  # ((Number of goals against) / (N of matches))
+  # 1. Fix scouts
+  # 2. Create features
+      # ((Number of goals scored) / (N of matches))  ## full schedule; last 3 rounds;
+      # ((Number of goals against) / (N of matches))
 
 library(dplyr)
 library(tidyr)
+
+
+# Fix scouts statistics
+cartola <- read.csv("db/2017/cartola_2017.csv", stringsAsFactors = FALSE)
+
+# What I have to do?
+temp.df <- data.frame(atletas.atleta_id = sort(rep(unique(cartola$atletas.atleta_id), length(unique(cartola$atletas.rodada_id)))),
+                      atletas.rodada_id = rep(0:(length(unique(cartola$atletas.rodada_id)) - 1)))
+
+
+
+# Create a data.frame to manipulate variables
+df <- cartola[, c(2,5,16:33)]
+
+# Convert NA values into zeroes
+df[,3:20] <- sapply(df[,3:20], function(x) ifelse(is.na(x), 0, x))
+
+  
+
+teste %>%
+  group_by(atletas.atleta_id) %>%
+  
+  
+
+df <- df %>%
+      arrange(atletas.atleta_id, - atletas.rodada_id) %>%
+      group_by(atletas.atleta_id) %>%
+      mutate_all(funs(abs(diff(c(0,.)))))
+
+
+df %>%
+  filter(atletas.atleta_id == "36540")
+    
+
 
 matches <- read.csv("db/2017/matches-brasileirao-2017.csv", stringsAsFactors = FALSE)
 

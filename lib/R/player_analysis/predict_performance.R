b88711f741405d4051c7d942879554016be73c5d
henriquepgomide <henriquepgomide@gmail.com>
2017-08-26 00:46:12 -0300

    Add data of round 21, and predictions of round 22

--- lib/R/player_analysis/predict_performance.R
+++ lib/R/player_analysis/predict_performance.R
@@ -23,10 +23,10 @@ df <- df[, -nzv]
 
 # Split training and validation
 treino <- df %>%
-  filter(!(Rodada == 20 & ano == 2017))
+  filter(!(Rodada == 21 & ano == 2017))
 
 validacao <- df %>%
-  filter(Rodada == 20 & ano == 2017)
+  filter(Rodada == 21 & ano == 2017)
 
 validacao <- validacao[complete.cases(validacao), ]
 

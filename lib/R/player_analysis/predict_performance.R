1463d5ce71bf701ea2cad1607bd0b077bf992fa2
henriquepgomide <henriquepgomide@gmail.com>
2017-10-27 08:39:20 -0200

    Add data from round 30 and aggregated data

--- lib/R/player_analysis/predict_performance.R
+++ lib/R/player_analysis/predict_performance.R
@@ -23,10 +23,10 @@ df <- df[, -nzv]
 
 # Split training and validation
 treino <- df %>%
-  filter(!(Rodada == 27 & ano == 2017))
+  filter(!(Rodada == 30 & ano == 2017))
 
 validacao <- df %>%
-  filter(Rodada == 27 & ano == 2017)
+  filter(Rodada == 30 & ano == 2017)
 
 validacao <- validacao[complete.cases(validacao), ]
 

5c2ebe0d84e373010748e895ea5ca55f613b159f
henriquepgomide <henriquepgomide@gmail.com>
2019-08-15 12:24:15 -0300

    fix round 13 data, update matches list, fix create_players_stats.R script

--- src/R/create_players_stats.R
+++ src/R/create_players_stats.R
@@ -3,74 +3,121 @@ library(tidyverse)
 library(zoo)
 
 setwd("~/caRtola")
-
 source("~/caRtola/src/R/utils.R")
+
 
 # 0. Dataprep -------------------------------------------------------------
 data <- readCsvInsideFolder("~/caRtola/data/2019/",
                             "rodada",
                             "2019")
 
-data$atletas.nome <- fixEncodingIssues(data$atletas.nome)
+# Fix encoding
+data$atletas.nome               <- fixEncodingIssues(data$atletas.nome)
 data$atletas.clube.id.full.name <- fixEncodingIssues(data$atletas.clube.id.full.name)
-data$atletas.status_id <- fixEncodingIssues(data$atletas.status_id)
+data$atletas.status_id          <- fixEncodingIssues(data$atletas.status_id)
 
 # Transform rodada_id into factor
 data$atletas.rodada_id  <- factor(data$atletas.rodada_id,
                                   levels = c(1:max(data$atletas.rodada_id)),
                                   ordered = TRUE)
 
-data <- dplyr::arrange(data, atletas.atleta_id, desc(atletas.rodada_id))
+# Convert team names with universal team ids
+mapTeamNames <- function (var) {
+  # Map team names using all year dictionary
+  temp1 <- read.csv("~/caRtola/data/times_ids.csv", stringsAsFactors = FALSE)
+  var <- plyr::mapvalues(var, 
+                         from = as.vector(temp1$nome.cartola), 
+                         to = as.vector(temp1$id))
+  rm(temp1)
+  return(var)
+}
 
-playerInfo <- 
+data$atletas.clube.id.full.name <-  mapTeamNames(data$atletas.clube.id.full.name)
+
+# Create column to flag players who really got into the game
+is_column_NA    <- sapply(data[, 16:33],  function(x) is.na(x))
+data$has.played <- apply(is_column_NA, 1, function(x) any(x==FALSE))
+rm(is_column_NA)
+
+# Subset data
+data <- 
+  data %>%
+  dplyr::arrange(atletas.atleta_id, atletas.rodada_id) %>%
+  dplyr::filter(atletas.posicao_id != "tec" & has.played == TRUE)
+
+# Create players list
+player_info <- 
   data %>%
   dplyr::select(atletas.slug, atletas.atleta_id, atletas.clube.id.full.name, 
                 atletas.posicao_id, atletas.rodada_id, atletas.pontos_num, 
                 atletas.status_id, atletas.apelido, atletas.preco_num,
                 atletas.variacao_num)
 
-scouts <- dplyr::select(data, 
-                        atletas.rodada_id, atletas.atleta_id, 
-                        CA, FC, 
-                        FS, GC, I,
-                        PE, RB, SG,
-                        FF, FD, G, 
-                        DD, GS, A,
-                        FT, CV, DP,
-                        PP)
+# Deaggregate scouts
+deaggregateScouts <- function (data) {
+  # Deaggregate scouts from Cartola Data - which is aggregated from the API
+  
+  # Helper function to lag over columns 
+  lagScouts <- function(x) {
+    return(x - lag(x, 1, default = NA))
+  }
+  
+  # Create deaggregated scouts 
+  scouts <-
+    data %>%
+    arrange(atletas.atleta_id, atletas.rodada_id) %>%
+    select(atletas.status_id, 
+           atletas.rodada_id, atletas.atleta_id, 
+           CA, FC, 
+           FS, GC, I,
+           PE, RB, SG,
+           FF, FD, G, 
+           DD, GS, A,
+           FT, CV, DP,
+           PP) 
+  
+  scouts[,4:21] <- sapply(scouts[,4:21], function(x) ifelse(is.na(x), 0, x))
+  
+  
+  col_names <- names(scouts)[4:21]
+  
+  deaggragate_scouts <- 
+    scouts %>%
+    group_by(atletas.atleta_id) %>%
+    mutate_at(.vars = col_names, .funs = lagScouts) %>%
+    fill(col_names, .direction = "up") %>%
+    filter(atletas.rodada_id != min(atletas.rodada_id))
+  
+  first_round_stats <- 
+    scouts %>%
+    group_by(atletas.atleta_id) %>%
+    filter(atletas.rodada_id == min(atletas.rodada_id))
+  
+  scouts <- rbind(deaggragate_scouts, first_round_stats)
+  scouts <- arrange(scouts, atletas.atleta_id, atletas.rodada_id)
+  
+  return(scouts)
+  
+}
 
-scouts[,3:20] <- sapply(scouts[,3:20], function(x) ifelse(is.na(x), 0, x))
-
-left_join(pl, scouts, by = ""
-
-# TODO - Filter scouts of players who haven't played yet.
-scouts <- 
-  scouts %>%
-  dplyr::group_by(atletas.atleta_id) %>%
-  dplyr::mutate_each(funs(abs(diff(c(.,0)))), -`atletas.rodada_id`) %>%
-  ungroup()
 
 # Join with proper scouts
-data <- left_join(playerInfo, scouts, by = c("atletas.atleta_id", "atletas.rodada_id"))
-
-temp1 <- read.csv("~/caRtola/data/times_ids.csv", stringsAsFactors = FALSE)
-
-data$atletas.clube.id.full.name <- plyr::mapvalues(data$atletas.clube.id.full.name, 
-                                             from = as.vector(temp1$nome.cartola), 
-                                             to = as.vector(temp1$id))
-rm(temp1)
+scouts <- deaggregateScouts(data)
+data <- left_join(player_info, scouts, by = c("atletas.atleta_id", "atletas.rodada_id"))
 
 # Validation  - Compute scores
-data$computed.score <- 
-   (data$CA * -2) + (data$FC * -0.5) + (data$RB * 1.5) +
-   (data$GC * -5) + (data$CV * -5)   + (data$SG * 5) +
-   (data$FS * .5) + (data$PE * -.3)  + (data$A * 5)  +
-   (data$FT * 3)  + (data$FD * 1.2)  + (data$FF * .8) +
-   (data$G * 8)   + (data$I * -.5)   + (data$PP * -4) +
-   (data$DD * 3)  + (data$DP * 7)    + (data$GS * -2)
+#data$computed.score <- 
+#   (data$CA * -2) + (data$FC * -0.5) + (data$RB * 1.5) +
+#   (data$GC * -5) + (data$CV * -5)   + (data$SG * 5) +
+#   (data$FS * .5) + (data$PE * -.3)  + (data$A * 5)  +
+#   (data$FT * 3)  + (data$FD * 1.2)  + (data$FF * .8) +
+#   (data$G * 8)   + (data$I * -.5)   + (data$PP * -4) +
+#   (data$DD * 3)  + (data$DP * 7)    + (data$GS * -2)
+## Plot scatter to check if scouts were properly handled
+#ggplot(data, aes(x = computed.score, y = atletas.pontos_num)) +
+#  geom_point()
 
 # 1. Feature Engineering ---------------------------------------------------
-
 # 2. Remove clean sheet bonus
 data$score.no.cleansheets <- 
    (data$CA * -2) + (data$FC * -0.5) + (data$RB * 1.5) +
@@ -91,21 +138,10 @@ names(data)[1:6] <- c("slug", "atleta.id", "team",
 
 data$rodada <- as.integer(data$rodadaF)
 
-data$status <- ifelse(data$rodada == 1 & data$pontuacao != 0,  "Provável", data$status)
-
-data <- dplyr::mutate(data, pontuou = ifelse(CA + FC + FS + 
-                                      GC + I + PE + 
-                                      RB + SG + FF + 
-                                      FD + G + DD + 
-                                      GS + A + FT + 
-                                      CV + DP + PP == 0,
-                                      FALSE, TRUE))
-
-# Get matches data
 matches <- read.csv("~/caRtola/data/2019/2019_partidas.csv", check.names = FALSE)
 
 convertMatchesToTidy <- function(dataframe) { 
-  # Convert and preprocess matches dataframe  
+  # Convert and preprocess matches dataframe 
   tidy_matches <- tidyr::gather(dataframe, `home_team`, `away_team`, value = "team_name", key = "home_away")
   tidy_matches <- dplyr::select(tidy_matches, date, home_away, team_name, round)
   tidy_matches$home_away <- gsub("_team", "", tidy_matches$home_away)
@@ -114,6 +150,9 @@ convertMatchesToTidy <- function(dataframe) {
 }
 
 matches <- convertMatchesToTidy(matches)
+matches <- 
+  matches %>%
+  distinct(home_away, team_name, round)
 
 # Merge matches and data frames to compute home and away scores
 data <- left_join(data, matches, 
@@ -123,7 +162,6 @@ data <- left_join(data, matches,
 scouts.mean <- 
   data %>%
   dplyr::group_by(atleta.id) %>% 
-  dplyr::filter(pontuou == TRUE) %>%
   dplyr::arrange(rodadaF) %>%
   dplyr::mutate_at(.vars = c("shotsX", "faltas", "RB", 
                       "PE", "A", "I",
@@ -148,20 +186,13 @@ createHomeAndAwayScouts <- function(){
   scouts.home.away <- 
     data %>%
     dplyr::group_by(atleta.id, home_away) %>%
-    dplyr::filter(pontuou == TRUE) %>%
     dplyr::arrange(rodadaF) %>%
     dplyr::mutate_at(.vars = c("score.no.cleansheets","pontuacao"), 
               .funs = cummean) %>%
-#    dplyr::select("atleta.id", "home_away", "rodada", 
-#                  "score.no.cleansheets","pontuacao") %>%
-    ungroup()
+    dplyr::select("atleta.id", "home_away", "rodada", 
+                  "score.no.cleansheets","pontuacao") 
   
   names(scouts.home.away)[4:5] <- paste0(names(scouts.home.away)[4:5], ".mean") 
-  
-  scouts.home.away <- 
-    scouts.home.away %>%
-    dplyr::group_by(atleta.id, home_away) %>%
-    dplyr::filter(rodada == max(rodada))
   
   scouts.home.away <- tidyr::gather(scouts.home.away, 
                              key = vars,
@@ -174,6 +205,10 @@ createHomeAndAwayScouts <- function(){
                             col = "var",
                             "vars", "home_away",
                             sep = ".")
+  
+#  scouts.home.away <- distinct(scouts.home.away, 
+#                    atleta.id, var, rodada, 
+#                    .keep_all = TRUE)
   
   scouts.home.away <- tidyr::spread(scouts.home.away,
                              key = var,
@@ -192,7 +227,6 @@ scouts.home.away <- createHomeAndAwayScouts()
 players.list <- 
   data %>%
   dplyr::group_by(atleta.id) %>%
-  dplyr::filter(pontuou == TRUE) %>%
   dplyr::summarise(n.jogos = n(),
                    rodada  = max(rodada)) # %>%
 #  dplyr::filter(n.jogos >= 1)
@@ -209,7 +243,7 @@ data <- left_join(data, n.games.scouts.home.away,
 df.model <- 
   data %>%
   dplyr::filter(atleta.id %in% players.list$atleta.id) %>%
-  dplyr::filter(rodada >= 1 & pontuou == TRUE) %>%
+  dplyr::filter(rodada >= 1) %>%
   dplyr::mutate(diff.home.away = score.no.cleansheets.mean.home - score.no.cleansheets.mean.away)
 
 df.model$diff.home.away.scalled <- scale(df.model$diff.home.away)
@@ -230,7 +264,7 @@ df.cartola.2019 <-
          PE_mean, A_mean, I_mean, 
          FS_mean, FF_mean, G_mean,
          DD_mean, DP_mean,
-         atletas.status_id, atletas.variacao_num, pontuacao)
+         atletas.status_id.x, atletas.variacao_num, pontuacao)
 
 names(df.cartola.2019) <- c("player_slug", "player_id", "player_nickname",
                             "player_team", "player_position", "price_cartoletas",
@@ -246,4 +280,3 @@ write.csv(df.cartola.2019,
           "~/caRtola/data/2019/2019-medias-jogadores.csv", 
           row.names = FALSE,
           na="0")
-  

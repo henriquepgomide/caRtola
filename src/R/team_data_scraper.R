ae473065fb82b61c4ec00c0099eb194d6c07c49f
henriquepgomide <henriquepgomide@gmail.com>
2019-09-03 22:46:33 -0300

    add round 17 data, aggregated scouts, team ranking and matches odds

--- src/R/team_data_scraper.R
+++ src/R/team_data_scraper.R
@@ -76,9 +76,10 @@ fetchMatchDetail <- function(round) {
 }
 
 # Write csv
-round_number <- 17
+round_number <- 18
 data <- fetchMatchDetail(round_number)
 data <- subset(data, data$round <= round_number)
+data <- subset(data, data$date < "2019-09-10")
 
 # Temp
 data <- filter(data, !(home_team == 276 & round == 15))

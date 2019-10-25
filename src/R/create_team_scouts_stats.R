35ea2654b5a08183d8b7a1f4ac6c820b5db09239
henriquepgomide <henriquepgomide@gmail.com>
2019-10-25 08:33:36 -0300

    add data from round 27 and predictions for round 28

--- src/R/create_team_scouts_stats.R
+++ src/R/create_team_scouts_stats.R
@@ -20,6 +20,7 @@ matches_19g <- gather(matches_19,
                        -round, -match_number)
 matches_19g$round <- factor(matches_19g$round, ordered = TRUE)
 matches_19g$team <- as.character(matches_19g$team)
+matches_19g$home_away <- gsub("_team", "", matches_19g$home_away)
 
 # Select variables for scouts
 data <- 
@@ -46,7 +47,7 @@ ggplot(pontos_homeaway_f,
        aes(x = rodadaF, y = pontuacao_mean, color = home_away, group = team)) + 
   geom_point() + 
   geom_smooth(aes(color = home_away, group = team)) + 
-  facet_wrap(home_away ~ team)
+  facet_wrap(~ team)
 
 
 # TODO --------------------------------------------------------------------
@@ -57,15 +58,40 @@ pontos_homeaway_f <-
   dplyr::group_by(team, home_away, rodadaF) %>%
   dplyr::arrange(team, home_away, rodadaF) %>%
   dplyr::summarize(pontuacao = sum(pontuacao)) %>%
-  dplyr::mutate(pontuacao_mean = cummean(pontuacao))
-  
+  dplyr::mutate(pontuacao_mean = cummean(pontuacao)) %>%
+  ungroup()
+ 
+pontos_homeaway_f$rodadaF <- as.character(pontos_homeaway_f$rodadaF)
+matches_19g$round <- as.character(matches_19g$round)
+
 test <- 
   left_join(matches_19g, pontos_homeaway_f, 
             by = c("home_away" = "home_away",
                    "team"      = "team",
                    "round"     = "rodadaF"))
+test <- 
+  test %>%
+  dplyr::select(-round)
+  
+teste <- 
+  gather(test,
+         key = "vars",
+         value = "value",
+         -home_away, -match_number)
 
+teste_2 <- unite(teste, 
+                 col = "vars",
+                 "home_away","vars", 
+                 sep = "_")
+
+teste_3 <- spread(teste_2, 
+                  key = "vars",
+                  value = "value",
+                  sep = "_", 
+                  convert = TRUE)
 
 
 # 2. Try to predict scores from a given round 
 #    based on team features
+
+ggplot(data = test, aes(x = pontuacao, color = home_away, fill = home_away)) + geom_density() + facet_wrap(~ team)

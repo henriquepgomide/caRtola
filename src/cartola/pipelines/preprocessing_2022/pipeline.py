149f999a63838df6a086d50a2c7c68271e5fc528
Arnaldo Gualberto <arnaldo.g12@gmail.com>
2022-07-28 23:00:58 -0300

    chore: rename some stuff

--- src/cartola/pipelines/preprocessing_2022/pipeline.py
+++ src/cartola/pipelines/preprocessing_2022/pipeline.py
@@ -5,14 +5,16 @@ generated using Kedro 0.18.2
 
 from kedro.pipeline import Pipeline, node, pipeline
 from .nodes import remove_nans
-from cartola.utils.dataframes import merge_partitioned_datasets
+from cartola.utils.dataframes import concat_partitioned_datasets
 
 
 def create_pipeline(**kwargs) -> Pipeline:
     return pipeline([
         node(
-            merge_partitioned_datasets,
+            concat_partitioned_datasets,
             inputs="raw_2022",
-            outputs="df_combined",
-        )
+            outputs="concat_2022",
+            tags=["preprocessing"],
+            namespace="2022",
+        ),
     ])
--- src/cartola/utils/dataframes.py
+++ src/cartola/utils/dataframes.py
@@ -2,10 +2,10 @@ from typing import Dict
 import pandas as pd
 
 
-def merge_partitioned_datasets(partitioned_dataset: Dict[str, pd.DataFrame]):
-    df_combined = pd.DataFrame()
+def concat_partitioned_datasets(partitioned_dataset: Dict[str, pd.DataFrame]):
+    df_concat = pd.DataFrame()
     for _, partition_load_func in partitioned_dataset.items():
         partition_data = partition_load_func()
-        df_combined = pd.concat([df_combined, partition_data], ignore_index=True)
+        df_concat = pd.concat([df_concat, partition_data], ignore_index=True)
 
-    return df_combined.reset_index(drop=True)
+    return df_concat.reset_index(drop=True)

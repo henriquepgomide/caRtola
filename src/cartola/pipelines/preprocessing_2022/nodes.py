4b3bf7e7c2c0d2f406bb70776a2d907544ec6271
Arnaldo Gualberto <arnaldo.g12@gmail.com>
2022-07-30 22:34:21 -0300

    feat: preprocessing pipeline for 2022

--- src/cartola/pipelines/preprocessing_2022/nodes.py
+++ src/cartola/pipelines/preprocessing_2022/nodes.py
@@ -2,6 +2,3 @@
 This is a boilerplate pipeline 'preprocessing_2022'
 generated using Kedro 0.18.2
 """
-
-def remove_nans(datasets):
-    pass
--- src/cartola/pipelines/preprocessing_2022/pipeline.py
+++ src/cartola/pipelines/preprocessing_2022/pipeline.py
@@ -4,17 +4,30 @@ generated using Kedro 0.18.2
 """
 
 from kedro.pipeline import Pipeline, node, pipeline
-from .nodes import remove_nans
-from cartola.utils.dataframes import concat_partitioned_datasets
+from cartola.commons.dataframes import concat_partitioned_datasets
+from cartola.commons.cartola import fill_scouts_with_zeros, fill_empty_slugs
 
 
 def create_pipeline(**kwargs) -> Pipeline:
-    return pipeline([
-        node(
-            concat_partitioned_datasets,
-            inputs="raw_2022",
-            outputs="concat_2022",
-            tags=["preprocessing"],
-            namespace="2022",
-        ),
-    ])
+    return pipeline(
+        [
+            node(
+                concat_partitioned_datasets,
+                inputs="raw_2022",
+                outputs="concat_2022",
+                tags=["preprocessing"],
+            ),
+            node(
+                fill_scouts_with_zeros,
+                inputs=["concat_2022", "params:scouts_2022"],
+                outputs="df_filled_scouts",
+                tags=["preprocessing"],
+            ),
+            node(
+                fill_empty_slugs,
+                inputs=["df_filled_scouts", "params:slug_col", "params:nickname_col"],
+                outputs="preprocessed_2022",
+                tags=["preprocessing"],
+            ),
+        ],
+    )

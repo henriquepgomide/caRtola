633576993a1cad089acf5bdaec7066e82362b433
Arnaldo Gualberto <arnaldo.g12@gmail.com>
2022-08-06 16:48:29 -0300

    ✨feat: 2021 preprocessing pipeline

--- src/cartola/pipeline_registry.py
+++ src/cartola/pipeline_registry.py
@@ -2,7 +2,7 @@
 from typing import Dict
 
 from kedro.pipeline import Pipeline, pipeline
-from cartola.pipelines import preprocessing as prep2022
+from cartola.pipelines import preprocessing
 
 
 def register_pipelines() -> Dict[str, Pipeline]:
@@ -11,11 +11,23 @@ def register_pipelines() -> Dict[str, Pipeline]:
     Returns:
         A mapping from a pipeline name to a ``Pipeline`` object.
     """
+    params_preprocessing = ["params:preprocessing.map_col_names", "params:preprocessing.map_status_id_to_str"]
+    pipe_2021 = pipeline(
+        pipe=preprocessing.create_pipeline(),
+        inputs=None,
+        namespace="2021",
+        parameters=params_preprocessing,
+    )
+    
     pipe_2022 = pipeline(
-        pipe=prep2022.create_pipeline(),
+        pipe=preprocessing.create_pipeline(),
         inputs=None,
         namespace="2022",
-        parameters=["params:preprocessing.map_col_names", "params:preprocessing.map_status_id_to_str"],
+        parameters=params_preprocessing,
     )
 
-    return {"__default__": pipe_2022, "2022": pipe_2022}
+    return {
+        "__default__": pipe_2021 + pipe_2022, 
+        "2021": pipe_2021,
+        "2022": pipe_2022
+    }

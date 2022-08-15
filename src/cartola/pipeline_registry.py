735bdd0b27195649e677ffd95a9228cb8cee217d
Arnaldo Gualberto <arnaldo.g12@gmail.com>
2022-08-15 16:35:59 -0300

    ✨ feat: add pipelines for 2017-2019

--- src/cartola/pipeline_registry.py
+++ src/cartola/pipeline_registry.py
@@ -13,6 +13,27 @@ def register_pipelines() -> Dict[str, Pipeline]:
         A mapping from a pipeline name to a ``Pipeline`` object.
     """
     params_preprocessing = ["params:preprocessing.map_col_names", "params:preprocessing.map_status_id_to_str"]
+    pipe_2017 = pipeline(
+        pipe=preprocessing.create_pipeline(),
+        inputs=None,
+        namespace="2017",
+        parameters=params_preprocessing,
+    )
+
+    pipe_2018 = pipeline(
+        pipe=preprocessing.create_pipeline(),
+        inputs=None,
+        namespace="2018",
+        parameters=params_preprocessing,
+    )
+
+    pipe_2019 = pipeline(
+        pipe=preprocessing.create_pipeline(),
+        inputs=None,
+        namespace="2019",
+        parameters=params_preprocessing,
+    )
+
     pipe_2020 = pipeline(
         pipe=preprocessing.create_pipeline(),
         inputs=None,
@@ -34,4 +55,12 @@ def register_pipelines() -> Dict[str, Pipeline]:
         parameters=params_preprocessing,
     )
 
-    return {"__default__": pipe_2020 + pipe_2021 + pipe_2022, "2020": pipe_2020, "2021": pipe_2021, "2022": pipe_2022}
+    return {
+        "__default__": pipe_2017 + pipe_2018 + pipe_2019 + pipe_2020 + pipe_2021 + pipe_2022,
+        "2017": pipe_2017,
+        "2018": pipe_2018,
+        "2019": pipe_2019,
+        "2020": pipe_2020,
+        "2021": pipe_2021,
+        "2022": pipe_2022,
+    }

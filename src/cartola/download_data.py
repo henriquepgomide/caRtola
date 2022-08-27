57244af35054fb033fcec3a777613dd1e86dd5ae
Arnaldo Gualberto <arnaldo.g12@gmail.com>
2022-08-26 21:47:27 -0300

    🐛 fix: column names to match previous files

--- src/cartola/download_data.py
+++ src/cartola/download_data.py
@@ -9,16 +9,16 @@ data_json = requests.get("https://api.cartolafc.globo.com/atletas/mercado").json
 
 df_atletas = pd.DataFrame(data_json["atletas"])
 df_atletas = df_atletas.join(pd.DataFrame(df_atletas.pop("scout").values.tolist()))
+df_atletas = df_atletas.rename(columns={col: f"atletas.{col}" if col.islower() else col for col in df_atletas.columns})
+df_atletas = df_atletas.drop(columns="atletas.gato_mestre")
 
 df_clubes = pd.DataFrame(data_json["clubes"].values())
-df_clubes = df_clubes.rename(columns=dict(id="clube_id", nome="nome_clube", abreviacao="clube_abreviacao"))
+df_clubes = df_clubes.rename(columns=dict(id="atletas.clube_id", nome="atletas.clube.id.full.name"))
+df_clubes = df_clubes[["atletas.clube_id", "atletas.clube.id.full.name"]]
 
-df_posicoes = pd.DataFrame(data_json["posicoes"].values())
-df_posicoes = df_posicoes.rename(columns=dict(id="posicao_id", nome="nome_posicao", abreviacao="posicao_abreviacao"))
+df_merge = df_atletas.merge(df_clubes, how="left", on="atletas.clube_id")
 
-df_merge = df_atletas.merge(df_clubes, how="left", on="clube_id").merge(df_posicoes, how="left", on="posicao_id")
-
-rodada = df_merge.rodada_id[0].astype(str)
+rodada = df_merge.loc[0, "atletas.rodada_id"].astype(str)
 year = date.today().year
 file = join("data", "01_raw", str(year), f"rodada-{rodada}.csv")
 

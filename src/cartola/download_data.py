b3d8f4b5334d464e4599de660f9e5b9b11f9a5ca
Arnaldo Gualberto <arnaldo.g12@gmail.com>
2022-08-29 11:52:24 -0300

    🩹 fix: sort data to avoid duplicated with same data

--- src/cartola/download_data.py
+++ src/cartola/download_data.py
@@ -23,5 +23,10 @@ year = date.today().year
 file = join("data", "01_raw", str(year), f"rodada-{rodada}.csv")
 print(file)
 
+cols_scouts = sorted([col for col in df_merge.columns if col.isupper()])
+cols_atleta = sorted(set(df_merge.columns) - set(cols_scouts))
+
+df_merge = df_merge.loc[:, cols_atleta + cols_scouts]
+df_merge.sort_values(by="atletas.atleta_id", inplace=True, ignore_index=True)
 df_merge.to_csv(file)
 # json.dump(data_json, open(file.replace(".csv", ".json"), "w", encoding="utf-8"), indent=2, ensure_ascii=False)

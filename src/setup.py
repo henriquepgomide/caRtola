0a0160405782d3da7ca9e5758f2a6c33804da26d
Arnaldo Gualberto <arnaldo.g12@gmail.com>
2022-08-31 22:16:56 -0300

    🚨chore: lint issues

--- src/setup.py
+++ src/setup.py
@@ -1,8 +1,6 @@
 from setuptools import find_packages, setup
 
-entry_point = (
-    "cartola = cartola.__main__:main"
-)
+entry_point = "cartola = cartola.__main__:main"
 
 
 # get the dependencies and installs

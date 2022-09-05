d1bddffe90122ae562db81dd5872023b083cf15f
Arnaldo Gualberto <arnaldo.g12@gmail.com>
2022-09-05 12:31:51 -0300

    feat: start pipeline to aggregate data

---
+++ src/cartola/pipelines/aggregate_all_years/nodes.py
@@ -0,0 +1,4 @@
+"""
+This is a boilerplate pipeline 'aggregate_all_years'
+generated using Kedro 0.18.2
+"""
---
+++ src/cartola/pipelines/aggregate_all_years/pipeline.py
@@ -0,0 +1,10 @@
+"""
+This is a boilerplate pipeline 'aggregate_all_years'
+generated using Kedro 0.18.2
+"""
+
+from kedro.pipeline import Pipeline, pipeline
+
+
+def create_pipeline(**kwargs) -> Pipeline:
+    return pipeline([])

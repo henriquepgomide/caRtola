4757009a77d646064036e7bcc3d80b13c8a08d0e
Arnaldo Gualberto <arnaldo.g12@gmail.com>
2024-06-15 16:15:45 -0300

    chore: dependencies and remove unused tests

--- src/tests/pipelines/aggregate_all_years/test_pipeline.py
+++
@@ -1,9 +0,0 @@
-"""
-This is a boilerplate test file for pipeline 'aggregate_all_years'
-generated using Kedro 0.18.2.
-Please add your pipeline tests here.
-
-Kedro recommends using `pytest` framework, more info about it can be found
-in the official documentation:
-https://docs.pytest.org/en/latest/getting-started.html
-"""

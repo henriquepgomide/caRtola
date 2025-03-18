705786bc38a5230f2da09c0024f9146664ae538e
Arnaldo Gualberto <arnaldo.g12@gmail.com>
2025-03-17 23:39:37 -0300

    fix: create directory for a new year

--- src/cartola/download_data.py
+++ src/cartola/download_data.py
@@ -1,3 +1,4 @@
+import os
 from datetime import date
 from os.path import exists, join
 
@@ -8,11 +9,17 @@ data_json = requests.get("https://api.cartolafc.globo.com/atletas/mercado").json
 
 df_atletas = pd.DataFrame(data_json["atletas"])
 df_atletas = df_atletas.join(pd.DataFrame(df_atletas.pop("scout").values.tolist()))
-df_atletas = df_atletas.rename(columns={col: f"atletas.{col}" if col.islower() else col for col in df_atletas.columns})
+df_atletas = df_atletas.rename(
+    columns={
+        col: f"atletas.{col}" if col.islower() else col for col in df_atletas.columns
+    }
+)
 df_atletas = df_atletas.drop(columns="atletas.gato_mestre")
 
 df_clubes = pd.DataFrame(data_json["clubes"].values())
-df_clubes = df_clubes.rename(columns=dict(id="atletas.clube_id", nome="atletas.clube.id.full.name"))
+df_clubes = df_clubes.rename(
+    columns=dict(id="atletas.clube_id", nome="atletas.clube.id.full.name")
+)
 df_clubes = df_clubes[["atletas.clube_id", "atletas.clube.id.full.name"]]
 
 df_merge = df_atletas.merge(df_clubes, how="left", on="atletas.clube_id")
@@ -29,5 +36,6 @@ df_merge = df_merge.loc[:, cols_atleta + cols_scouts]
 df_merge.sort_values(by="atletas.atleta_id", inplace=True, ignore_index=True)
 
 if not exists(file):
+    os.makedirs(os.path.dirname(file), exist_ok=True)
     df_merge.to_csv(file)
     # json.dump(data_json, open(file.replace(".csv", ".json"), "w", encoding="utf-8"), indent=2, ensure_ascii=False)
